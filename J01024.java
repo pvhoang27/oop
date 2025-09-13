5756d5bf10d227805182d785b89969c6fe1ec387
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-09-13 18:35:24 +0700

    update 13/09/2025

--- J01024.java
+++ J01024.java
@@ -1,4 +1,19 @@
 import java.util.*;
 public class J01024 {
-  
+  static String solve(String s){
+    for(int i = 0 ; i < s.length(); i++){
+      if(s.charAt(i) > '2')
+        return "NO";
+    }
+    return "YES";
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t--> 0){
+      String s = sc.next();
+      System.out.println(solve(s));
+    }
+
+  }
 }
